819cbc2131325695cc8c3f730d2cb45f56c1e70c
storyfeet <thebeaky@psmail.net>
2018-10-30 20:50:33 +0000

    print now works properly

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ extern crate mksvg;
 use lazyf::{SGetter,Cfg};
 use mksvg::{Card,SvgWrite,Args,SvgArg};
 
-#[derive(Clone)]
+#[derive(Clone,Debug)]
 pub struct ImgCard{
     img:String,
 }
@@ -38,19 +38,25 @@ fn main() {
     let counts = cfg.get_s(("-nn","img_count")).unwrap_or("1".to_string());
     let out_base = cfg.get_s(("-out","out")).unwrap_or("out/cards".to_string());
 
+    let do_pdf = cfg.get_s(("-pdf","pdf"));
+
     let counts:Vec<i32> = counts.split(':').map(|s|s.parse().unwrap_or(1)).collect();
 
     let mut cards = Vec::new();
 
     for (i,img) in imgs.split(':').enumerate() {
         let cn = i % counts.len();
-        for _ in 0..cn{
+        for _ in 0..counts[cn]{
             cards.push(ImgCard::new(img.to_string()));
         }
     }
+    println!("CARDS:{:?}",cards);
 
     let pages = mksvg::page::pages_a4(out_base,w,h,&cards);
 
+    if let Some(pdpath) = do_pdf{
+        mksvg::page::unite_as_pdf(pages,pdpath);
+    }
 }
 
 
