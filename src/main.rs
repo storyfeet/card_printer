b80820e2deba5c5e4666680e0c2af74a4f37cf81
storyfeet <thebeaky@psmail.net>
2018-11-05 22:52:50 +0000

    now uses help messages

--- src/main.rs
+++ src/main.rs
@@ -29,10 +29,10 @@ impl Card<f64> for ImgCard{
 fn main() {
     let mut cfg = config("-c",&["conf.lz"]);
 
-    let w:usize = cfg.grab().fg("-w").cf("width").t().unwrap_or(4);
+    let w:usize = cfg.grab().fg("-w").cf("width").help("Columns").t().unwrap_or(4);
     let h:usize = cfg.grab().fg("-h").cf("height").t().unwrap_or(4);
 
-    let imgs = cfg.grab().fg("-imgs").cf("images").s().unwrap();//no img no svg
+    let imgs_ = cfg.grab().fg("-imgs").cf("images").s_req("Image names needed ':' separated in quotes");
     let counts = cfg.grab().fg("-nn").cf("img_count")
                     .s().unwrap_or("1".to_string());
 
@@ -41,14 +41,16 @@ fn main() {
 
     let do_pdf = cfg.grab().fg("-pdf").cf("pdf").s();
 
-    let landscape = cfg.grab().fg("-landscape").cf("landscape").is_present();
+    let landscape = cfg.grab().fg("-landscape").cf("landscape").help("Make landscape").is_present();
 
 
     let counts:Vec<i32> = counts.split(':').map(|s|s.parse().unwrap_or(1)).collect();
 
+    if cfg.help("Card Printer") {return};
+
     let mut cards = Vec::new();
 
-    for (i,img) in imgs.split(':').enumerate() {
+    for (i,img) in imgs_.unwrap().split(':').enumerate() {
         let cn = i % counts.len();
         for _ in 0..counts[cn]{
             cards.push(ImgCard::new(img.to_string()));
